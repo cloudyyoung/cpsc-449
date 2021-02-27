54a4e0e5a42dfdf7ca0dd81593f5d09eea4aaad8
Cloudy Young <cloudyyoung74@outlook.com>
2021-02-27 14:30:26 +0800

    Update Moves.hs

--- checkers/src/Moves.hs
+++ checkers/src/Moves.hs
@@ -27,14 +27,14 @@ jump_moves g = case (status g) of
     GameOver     -> []
   where
     jump_over [] = [[]]
-    jump_over z = z
+    jump_over x = x
     jump_piece xs = [(x,y):ys | (x,y) <- xs, ys <- jump_piece' (x,y) [] (x,y)]
     jump_piece' start rem (x,y) = undefined
     jump_king xs = [(x,y):ys | (x,y) <- xs, ys <- jump_king' (x,y) [] (x,y)]
     jump_king' start rem (x,y) = [(x'',y''):ys | ((x',y'),(x'',y'')) <- [((x + 1,y + 1),(x + 2,y + 2)),((x - 1,y + 1),(x - 2,y + 2)),((x + 1,y - 1),(x + 2,y - 2)),((x - 1,y - 1),(x - 2,y - 2))], not(elem (x',y') rem), opponent_occupied (x',y') g, start == (x'',y''), notoccupied (x'',y'') g, onboard (x'',y''), ys <- jump_over (jump_king' start ((x',y'):rem) (x'',y''))]
 
 
-notoccupied :: Cord -> GameState -> Bool
+notoccupied :: Coord -> GameState -> Bool
 notoccupied (x,y) g
     | elem (x,y) (blackPieces g)  = False
     | elem (x,y) (blackKings g)   = False
@@ -47,10 +47,10 @@ onboard (x,y) = (0 <= x) && (x<=7) && (0 <= y) && (y <= 7)
 
 opponent_occupied :: Coord -> GameState -> Bool
 opponent_occupied (x,y) g
-    | (status g == Red) && (elem (x,y) (blackPieces g)) = True
-    | (status g == Red) && (elem (x,y) (blackKings g))  = True
-    | (status g == Black) && (elem (x,y) (redPieces g)) = True
-    | (status g == Black) && (elem (x,y) (redKings g))  = True
-    | otherwise                                         = False
+    | (status g == (Turn Red)) && (elem (x,y) (blackPieces g))  = True
+    | (status g == (Turn Red)) && (elem (x,y) (blackKings g))   = True
+    | (status g == (Turn Black)) && (elem (x,y) (redPieces g))  = True
+    | (status g == (Turn Black)) && (elem (x,y) (redKings g))   = True
+    | otherwise                                                 = False
 
 
