642a6bcff2a03b6a74d86dbbf4b45108265888c8
Cloudy Young <cloudyyoung74@outlook.com>
2021-02-27 23:47:12 +0800

    Update ApplyMove.hs

--- checkers/src/ApplyMove.hs
+++ checkers/src/ApplyMove.hs
@@ -6,8 +6,6 @@ import Checkers.Types
 
 apply_move :: Move -> GameState -> GameState
 apply_move move g
-    | null (redKings g) && null (redPieces g)           = g {status = GameOver, message = "Black wins"}
-    | null (blackKings g) && null (blackPieces g)       = g {status = GameOver, message = "Red wins"}
     | elem move (jump_moves g)                          = make_jump_move move g
     | elem move (simple_moves g) && null (jump_moves g) = make_simple_move move g
     | otherwise                                         = g {message = "Illegal move! A jump is available:  " ++ show (jump_moves g)}
@@ -26,30 +24,24 @@ make_simple_move [pc_start,pc_end] g
     | (status g) == (Turn Red)   && elem start (redKings g)                                 = g {redKings    = replace start end (redKings g),                                 status = change_player (status g), message = "", history = [pc_start,pc_end]:(history g)}
     | (status g) == (Turn Red)   && elem start (redPieces g)   && is_king end (Turn Red)    = g {redPieces   = remove  start (redPieces g),   redKings   = end:(redKings g),   status = change_player (status g), message = "", history = [pc_start,pc_end]:(history g)}
     | (status g) == (Turn Red)   && elem start (redPieces g)                                = g {redPieces   = replace start end (redPieces g),                                status = change_player (status g), message = "", history = [pc_start,pc_end]:(history g)}
-    | otherwise                                                                             = g {message = "Invalid simple move"}
+    | otherwise                                                                             = g {message     = "Invalid simple move"}
     where
         start = porkcoord_to_coord pc_start
         end   = porkcoord_to_coord pc_end
 
 
 make_jump_move :: Move -> GameState -> GameState
-make_jump_move (start:next:rest) g = g
-    -- | status g == (Turn Black) && elem start (blackKings g) = undefined
-    -- | status g == (Turn Black) && elem start (blackPieces g)
-    --     =   if is_king next (Turn Black)
-    --         then undefined
-    --         else undefined
-    -- | status g == (Turn Red) && elem start (redKings g)
-    --     = make_jump_move (next:rest) (g{blackKings = remove (jumped start next) (blackKings g),
-    --                                     blackPieces = remove (jumped start next) (blackPieces g),
-    --                                     redKings = next:(remove start (redKings g)),
-    --                                     message = ""})
-    -- | status g == (Turn Red) && elem start (redPieces g)
-    --     =   if is_king next (Turn Red)
-    --         then undefined
-    --         else undefined
-    -- | otherwise = g{message = "invalid make_jump_move"}
-
+make_jump_move (pc_start:pc_next:rest) g = g
+    -- | (status g) == (Turn Black) && elem start (blackKings g)                               = undefined
+    -- | (status g) == (Turn Black) && elem start (blackPieces g) && is_king next (Turn Black) = undefined
+    -- | (status g) == (Turn Black) && elem start (blackPieces g)                              = undefined
+    -- | (status g) == (Turn Red)   && elem start (redKings g)                                 = make_jump_move (next:rest) (g {blackKings = remove (jumped start next) (blackKings g), blackPieces = remove (jumped start next) (blackPieces g), redKings = next:(remove start (redKings g)), message = ""})
+    -- | (status g) == (Turn Red)   && elem start (redPieces g)   && is_king next (Turn Red)   = undefined
+    -- | (status g) == (Turn Red)   && elem start (redPieces g)                                = undefined
+    -- | otherwise                                                                             = g {message = "invalid make_jump_move"}
+    -- where
+    --     start = porkcoord_to_coord pc_start
+    --     next  = porkcoord_to_coord pc_next
 
 -- is_king is defined in Moves.hs
 
@@ -58,11 +50,11 @@ replace _ _ [] = []
 replace x y zs = [if (z == x) then y else z | z <- zs]
 
 remove :: (Eq a) => a -> [a] -> [a]
-remove _ []  = []
+remove _ [] = []
 remove x ys = [y | y <- ys, y /= x]
 
 change_player :: Status -> Status
 change_player status = case status of
-                    (Turn Red)   -> (Turn Black)
-                    (Turn Black) -> (Turn Red)
-                    GameOver     -> GameOver
+                        (Turn Red)   -> (Turn Black)
+                        (Turn Black) -> (Turn Red)
+                        GameOver     -> GameOver
