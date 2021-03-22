becf680312c8ab6a6d74355a0eced3e73485d86d
Cloudy Young <cloudyyoung74@outlook.com>
2021-03-22 14:37:21 +0800

    Update Heuristic.hs

--- checkers/src/Heuristic.hs
+++ checkers/src/Heuristic.hs
@@ -7,7 +7,7 @@ import Checkers.Types
 
 
 black_heuristic:: GameState -> Int
-black_heuristic g = undefined
+black_heuristic g = length(blackPieces g) - length(redPieces g) + 2 * (length(blackKings g) - length(redKings g))
 
 red_heuristic::GameState -> Int
-red_heuristic g = undefined
+red_heuristic g = length(redPieces g) - length(blackPieces g) + 2 * (length(redKings g) - length(blackKings g))
