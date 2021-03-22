d9faadbe7b90c588c7160f873323e70bd75204ec
Cloudy Young <cloudyyoung74@outlook.com>
2021-03-22 19:16:26 +0800

    Update ABsearch.hs

--- checkers/src/ABsearch.hs
+++ checkers/src/ABsearch.hs
@@ -19,15 +19,22 @@ top = 30000
 bot :: Int
 bot = -30000
 
+children :: GameState -> [GameState]
+children g = foldr (\move xs -> (apply_move move g):xs) [] moves'
+    where
+        moves' = if (snd (moves g)) == []
+                    then fst (moves g)
+                    else snd (moves g)
+
+
 abmaxprune :: GameState -> (Int, Int) -> Int -> Int
 abmaxprune g (a, b) d
-    | a == b                        = a
-    | (moves g) == ([],[]) || d == 0     = if (status g) == Turn Red
-                                        then min (max (red_heuristic g) a) b
-                                        else min (max (black_heuristic g) a) b
-    | otherwise                     = abmaxprune' (children g) (a, b) d
+    | a == b                            = a
+    | (moves g) == ([],[]) || d == 0    = if (status g) == Turn Red
+                                            then min (max (red_heuristic g) a) b
+                                            else min (max (black_heuristic g) a) b
+    | otherwise                         = abmaxprune' (children g) (a, b) d
     where
-        children g = foldr (\move xs -> (apply_move move g):xs) [] (moves g)
         abmaxprune' [] (a, b) d = a
         abmaxprune' (g:gs) (a, b) d = abmaxprune' gs (a', b) d
             where
@@ -37,12 +44,11 @@ abmaxprune g (a, b) d
 abminprune :: GameState -> (Int, Int) -> Int -> Int
 abminprune g (a, b) d
     | a == b                    = b
-    | (moves g) == ([],[]) || d == 0   = if (status g) == Turn Red
-                                    then min (max (red_heuristic g) a) b
-                                    else min (max (black_heuristic g) a) b
-    | otherwise                 = abminprune' (children g) (a, b) d
+    | (moves g) == ([],[]) || d == 0    = if (status g) == Turn Red
+                                            then min (max (red_heuristic g) a) b
+                                            else min (max (black_heuristic g) a) b
+    | otherwise                         = abminprune' (children g) (a, b) d
     where
-        children g = foldr (\move xs -> (apply_move move g):xs) [] (moves g)
         abminprune' [] (a, b) d = b
         abminprune' (g:gs) (a, b) d = abminprune' gs (a, b') d
             where
@@ -50,13 +56,11 @@ abminprune g (a, b) d
 
 
 minimax :: GameState -> Int -> Status -> Int
-minimax g depth status
-    | depth == 0 || (moves g) == ([],[]) =   if status == Turn Red
-                                        then red_heuristic g
-                                        else black_heuristic g
-    | otherwise =                       if (status g) == status
-                                        then maximum(map (\g -> minimax g (depth - 1) status) (children g))
-                                        else minimum(map (\g -> minimax g (depth - 1) status) (children g))
-    where
-        children g = foldr (\move xs -> (apply_move move g):xs) [] (moves g)
+minimax g d s
+    | d == 0 || (moves g) == ([],[])    = if s == Turn Red
+                                            then red_heuristic g
+                                            else black_heuristic g
+    | otherwise                         = if (status g) == s
+                                            then maximum(map (\g -> minimax g (d - 1) s) (children g))
+                                            else minimum(map (\g -> minimax g (d - 1) s) (children g))
 
