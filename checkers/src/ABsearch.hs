dc27d005a640fc92fa570c59072babfa7d7d1dc7
Cloudy Young <cloudyyoung74@outlook.com>
2021-03-22 17:26:52 +0800

    Update

--- checkers/src/ABsearch.hs
+++ checkers/src/ABsearch.hs
@@ -23,3 +23,15 @@ abmaxprune g (a, b) d = undefined
 
 abminprune :: GameState -> (Int, Int) -> Int -> Int
 abminprune g (a, b) d = undefined
+
+minimax :: GameState -> Int -> Status -> Int
+minimax g depth status
+        | depth == 0 || (moves g) == [] =   if status == Turn Red
+                                            then red_hueristic g
+                                            else black_hueristic g
+        | otherwise =                       if (status g) == status
+                                            then maximum(map (\g -> minimax g (depth - 1) status) (children g))
+                                            else minimum(map (\g -> minimax g (depth - 1) status) (children g))
+    where
+        children g = foldr (\move xs -> (apply_move move g):xs) [] (moves g)
+
--- checkers/src/Heuristic.hs
+++ checkers/src/Heuristic.hs
@@ -6,8 +6,8 @@ import Checkers.Types
 
 
 
-black_heuristic:: GameState -> Int
+black_heuristic :: GameState -> Int
 black_heuristic g = length(blackPieces g) - length(redPieces g) + 2 * (length(blackKings g) - length(redKings g))
 
-red_heuristic::GameState -> Int
+red_heuristic :: GameState -> Int
 red_heuristic g = length(redPieces g) - length(blackPieces g) + 2 * (length(redKings g) - length(blackKings g))
