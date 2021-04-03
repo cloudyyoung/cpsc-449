06407828d71df7cbab7090177f6e43eca5befd45
Cloudy Young <cloudyyoung74@outlook.com>
2021-04-03 12:28:20 +0800

    Update submission.pl

--- exercise-3/submission.pl
+++ exercise-3/submission.pl
@@ -49,7 +49,7 @@ count(X, [_|Ls], Y) :- count(X, Ls, Y).
 
 % X is a contiguous sublist of Y
 substring([], _) :- !.
-substring(X,Y) :- myappend(X,_,T), myappend(_,T,Y), X \= [].
+substring(X,Y) :- myappend(X,_,T), myappend(_,T,Y).
 
 
 %%%%%%% Q4 %%%%%%%
@@ -152,7 +152,7 @@ path(X, X).
 path(X, Y) :- edge(X,Y); edge(X, Z), edge(Z, Y).
 
 % What is the length L of the shortest path from X to Y
-% shortpath(X, Y, L).
+% shortpath(X, Y, L)
 
 
 %%%%%%% Q8 %%%%%%%
@@ -191,6 +191,88 @@ wine.
 % Implement and define the following two predicates, where O is the output.
 % hamster_owner(O).
 % orange_juice_drinker(O).
+
+structure(houses,([house(_,_,_,_,_),house(_,_,_,_,_),house(_,_,_,_,_),house(_,_,_,_,_),house(_,_,_,_,_)])).
+
+nationality((Nationality, _, _, _, _), Nationality).
+color((_, Color, _, _, _), Color).
+sport((_, _, Sport, _, _), Sport).
+pet((_,_, _, Pet, _), Pet).
+drink((_, _, _, _, Drink), Drink).
+
+nextto(A, B, [A, B, _, _, _]).
+nextto(A, B, [_, A, B, _, _]).
+nextto(A, B, [_, _, A, B, _]).
+nextto(A, B, [_, _, _, A, B]).
+nextto(A, B, H) :- rightof(A, B, H).
+
+rightof(A, B, [B, A, _, _, _]).
+rightof(A, B, [_, B, A, _, _]).
+rightof(A, B, [_, _, B, A, _]).
+rightof(A, B, [_, _, _, B, A]).
+
+anywhere(X, H) :- mymember(X, H).
+
+% The Irishman lives in the first house on the left
+hint1([X, _, _, _, _]) :- nationality(X, irishman).
+
+% The man who plays baseball lives in the house next to the man who keeps a tiger
+hint2(H) :- sport(X, baseball), nextto(X, Y, H), pet(Y, tiger).
+
+% The occupant of the house, next to the house where the owner rides a horse, plays soccer.
+hint3(H) :- sport(X, soccer), pet(Y, horse), nextto(X, Y, H).
+
+% The squash player drinks gin
+hint4(H) :- sport(X, squash), drink(X, gin), anywhere(X, H).
+
+% The Frenchman plays rugger
+hint5(H) :- nationality(X, frenchman), sport(X, rugger), anywhere(X, H).
+
+% The Irishman lives next to the blue house
+hint6(H) :- nationality(X, irishman), nextto(X, Y, H), color(Y, blue).
+
+% The Englishman lives in the red house
+hint7(H) :- nationality(X, englishman), color(X, red), anywhere(X, H).
+
+% The Spaniard is often seen taking his dog for a walk
+hint8(H) :- nationality(spaniard), pet(X, dog), anywhere(X, H).
+
+% Beer is brewed (and drunk in large quantities) in the green house
+hint9(H) :- drink(H, beer), color(X, green), anywhere(X, H).
+
+% The Scotsman drinks whiskey and is often tipsey
+hint10(H) :- nationality(X, scotsman), drink(X, whiskey), anywhere(X, H).
+
+% The green house is immediately to the rightof of the white house
+hint11(H) :- color(X, green), color(Y, white), rightof(X, Y, H).
+
+% The tennis player owns snakes
+hint12(H) :- sport(X, tennis), pet(X, snack), anywhere(X, H).
+
+% Soccer is played in the yellow house
+hint13(H) :- sport(X, soccer), color(X, yellow), anywhere(X, H).
+
+% A lot of wine get consumed in the middle house
+hint14([_, _, X, _, _]) :- drink(X, wine).
+
+hints(H) :-
+    hint1(H), 
+    hint2(H), 
+    hint3(H), 
+    hint4(H), 
+    hint5(H), 
+    hint6(H), 
+    hint7(H), 
+    hint8(H), 
+    hint9(H), 
+    hint10(H), 
+    hint11(H), 
+    hint12(H), 
+    hint13(H),
+    hint14(H).
+
+hamster_owner(O) :- hints(houses), pet(X, hamster), nationality(X, O).
+orange_juice_drinker(O) :- hints(house), drink(X, orange_juice), nationality(X, O).
 
 %%%%%%% Q9 %%%%%%%
 
